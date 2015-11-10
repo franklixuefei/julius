d23b6707a87f1662a52346250f2164072f5bf329
Frank Li <frankvsense@gmail.com>
2015-11-10 16:45:50 -0500

    Implements non-loop julius

--- julius-simple/julius-simple.c
+++ julius-simple/julius-simple.c
@@ -1,82 +1,5 @@
-/**
- * @file   julius-simple.c
- * 
- * <EN>
- * @brief  Sample code for JuliusLib
- *
- * This is a simple code to link JuliusLib and do recognition.
- * 
- * This program will output recognition result to stdout according
- * to the given jconf setting.
- * </EN>
- * 
- * <JA>
- * @brief  JuliusLib用サンプルコード
- *
- * このコードは JuliusLib をリンクして簡単な認識器を構築します．
- *
- * このプログラムは音声入力に対して与えられた jconf 設定のもとで
- * 認識を行い，結果を標準出力に出力します．
- * 
- * </JA>
- * 
- * @author Akinobu Lee
- * @date   Tue Dec 11 14:40:04 2007
- * 
- * $Revision: 1.5 $
- * 
- */
-
-/* include top Julius library header */
 #include <julius/juliuslib.h>
 
-/** 
- * Callback to be called when start waiting speech input. 
- * 
- */
-static void
-status_recready(Recog *recog, void *dummy)
-{
-  if (recog->jconf->input.speech_input == SP_MIC || recog->jconf->input.speech_input == SP_NETAUDIO) {
-    fprintf(stderr, "<<< please speak >>>");
-  }
-}
-
-/** 
- * Callback to be called when speech input is triggered.
- * 
- */
-static void
-status_recstart(Recog *recog, void *dummy)
-{
-  if (recog->jconf->input.speech_input == SP_MIC || recog->jconf->input.speech_input == SP_NETAUDIO) {
-    fprintf(stderr, "\r                    \r");
-  }
-}
-
-/** 
- * Sub function to output phoneme sequence.
- * 
- */
-static void
-put_hypo_phoneme(WORD_ID *seq, int n, WORD_INFO *winfo)
-{
-  int i,j;
-  WORD_ID w;
-  static char buf[MAX_HMMNAME_LEN];
-
-  if (seq != NULL) {
-    for (i=0;i<n;i++) {
-      if (i > 0) printf(" |");
-      w = seq[i];
-      for (j=0;j<winfo->wlen[w];j++) {
-	center_name(winfo->wseq[w][j]->name, buf);
-	printf(" %s", buf);
-      }
-    }
-  }
-  printf("\n");  
-}
 /** 
  * Callback to output final recognition result.
  * This function will be called just after recognition of an input ends
@@ -85,154 +8,68 @@ put_hypo_phoneme(WORD_ID *seq, int n, WORD_INFO *winfo)
 static void
 output_result(Recog *recog, void *dummy)
 {
-  int i, j;
-  int len;
   WORD_INFO *winfo;
   WORD_ID *seq;
   int seqnum;
-  int n;
+  int n, num;
   Sentence *s;
   RecogProcess *r;
-  HMM_Logical *p;
-  SentenceAlign *align;
 
-  /* all recognition results are stored at each recognition process
-     instance */
+  char buf[4096];
+  int pos;
+
+  // constructs the JSON representation of the result data.
+  pos = 0;
+  pos += sprintf(&buf[pos], "{\"processes\":[");
   for(r=recog->process_list;r;r=r->next) {
-
-    /* skip the process if the process is not alive */
-    if (! r->live) continue;
-
-    /* result are in r->result.  See recog.h for details */
-
-    /* check result status */
-    if (r->result.status < 0) {      /* no results obtained */
-      /* outout message according to the status code */
-      switch(r->result.status) {
-      case J_RESULT_STATUS_REJECT_POWER:
-	printf("<input rejected by power>\n");
-	break;
-      case J_RESULT_STATUS_TERMINATE:
-	printf("<input teminated by request>\n");
-	break;
-      case J_RESULT_STATUS_ONLY_SILENCE:
-	printf("<input rejected by decoder (silence input result)>\n");
-	break;
-      case J_RESULT_STATUS_REJECT_GMM:
-	printf("<input rejected by GMM>\n");
-	break;
-      case J_RESULT_STATUS_REJECT_SHORT:
-	printf("<input rejected by short input>\n");
-	break;
-      case J_RESULT_STATUS_REJECT_LONG:
-	printf("<input rejected by long input>\n");
-	break;
-      case J_RESULT_STATUS_FAIL:
-	printf("<search failed>\n");
-	break;
-      }
-      /* continue to next process instance */
-      continue;
-    }
-
-    /* output results for all the obtained sentences */
+    if (!r->live) continue;
+    if (r->config->successive.enabled && r->result.status < 0 && r->config->output.progout_flag) continue;
+    if (r->result.status < 0) continue;
+    pos += sprintf(&buf[pos], "{\"sentences\":[");
     winfo = r->lm->winfo;
+    num = r->result.sentnum;
 
-    for(n = 0; n < r->result.sentnum; n++) { /* for all sentences */
-
+    for(n=0;n<num;n++) {
+      if (r->config->successive.enabled && r->config->output.progout_flag) break;
+      pos += sprintf(&buf[pos], "[");
       s = &(r->result.sent[n]);
       seq = s->word;
       seqnum = s->word_num;
-
-      /* output word sequence like Julius */
-      printf("sentence%d:", n+1);
-      for(i=0;i<seqnum;i++) printf(" %s", winfo->woutput[seq[i]]);
-      printf("\n");
-      /* LM entry sequence */
-      printf("wseq%d:", n+1);
-      for(i=0;i<seqnum;i++) printf(" %s", winfo->wname[seq[i]]);
-      printf("\n");
-      /* phoneme sequence */
-      printf("phseq%d:", n+1);
-      put_hypo_phoneme(seq, seqnum, winfo);
-      printf("\n");
-      /* confidence scores */
-      printf("cmscore%d:", n+1);
-      for (i=0;i<seqnum; i++) printf(" %5.3f", s->confidence[i]);
-      printf("\n");
-      /* AM and LM scores */
-      printf("score%d: %f", n+1, s->score);
-      if (r->lmtype == LM_PROB) { /* if this process uses N-gram */
-	printf(" (AM: %f  LM: %f)", s->score_am, s->score_lm);
-      }
-      printf("\n");
-      if (r->lmtype == LM_DFA) { /* if this process uses DFA grammar */
-	/* output which grammar the hypothesis belongs to
-	   when using multiple grammars */
-	if (multigram_get_all_num(r->lm) > 1) {
-	  printf("grammar%d: %d\n", n+1, s->gram_id);
-	}
-      }
       
-      /* output alignment result if exist */
-      for (align = s->align; align; align = align->next) {
-	printf("=== begin forced alignment ===\n");
-	switch(align->unittype) {
-	case PER_WORD:
-	  printf("-- word alignment --\n"); break;
-	case PER_PHONEME:
-	  printf("-- phoneme alignment --\n"); break;
-	case PER_STATE:
-	  printf("-- state alignment --\n"); break;
-	}
-	printf(" id: from  to    n_score    unit\n");
-	printf(" ----------------------------------------\n");
-	for(i=0;i<align->num;i++) {
-	  printf("[%4d %4d]  %f  ", align->begin_frame[i], align->end_frame[i], align->avgscore[i]);
-	  switch(align->unittype) {
-	  case PER_WORD:
-	    printf("%s\t[%s]\n", winfo->wname[align->w[i]], winfo->woutput[align->w[i]]);
-	    break;
-	  case PER_PHONEME:
-	    p = align->ph[i];
-	    if (p->is_pseudo) {
-	      printf("{%s}\n", p->name);
-	    } else if (strmatch(p->name, p->body.defined->name)) {
-	      printf("%s\n", p->name);
-	    } else {
-	      printf("%s[%s]\n", p->name, p->body.defined->name);
-	    }
-	    break;
-	  case PER_STATE:
-	    p = align->ph[i];
-	    if (p->is_pseudo) {
-	      printf("{%s}", p->name);
-	    } else if (strmatch(p->name, p->body.defined->name)) {
-	      printf("%s", p->name);
-	    } else {
-	      printf("%s[%s]", p->name, p->body.defined->name);
-	    }
-	    if (r->am->hmminfo->multipath) {
-	      if (align->is_iwsp[i]) {
-		printf(" #%d (sp)\n", align->loc[i]);
-	      } else {
-		printf(" #%d\n", align->loc[i]);
-	      }
-	    } else {
-	      printf(" #%d\n", align->loc[i]);
-	    }
-	    break;
-	  }
-	}
-	
-	printf("re-computed AM score: %f\n", align->allscore);
-
-	printf("=== end forced alignment ===\n");
+      int i;
+      for (i = 0; i < seqnum; ++i) {
+        pos += sprintf(&buf[pos], "{");
+        pos += sprintf(&buf[pos], "\"word\":");
+        if (seq != NULL) {
+          pos += sprintf(&buf[pos], "\"%s\"", winfo->woutput[seq[i]]);
+        } else {
+          pos += sprintf(&buf[pos], "\"null\"");
+        }
+#ifdef CONFIDENCE_MEASURE
+#ifndef CM_MULTIPLE_ALPHA
+        pos += sprintf(&buf[pos], ",");
+        pos += sprintf(&buf[pos], "\"confidence\":");
+        if (s->confidence != NULL) {
+          pos += sprintf(&buf[pos], "\"%5.3f\"", s->confidence[i]);
+        } else {
+          pos += sprintf(&buf[pos], "\"null\"");
+        }
+        pos += sprintf(&buf[pos], "}");
+#endif
+#endif
+        if (i != seqnum - 1) {
+          pos += sprintf(&buf[pos], ",");
+        }
       }
+      pos += sprintf(&buf[pos], "]");
     }
+    pos += sprintf(&buf[pos], "]}");
   }
+  pos += sprintf(&buf[pos], "]}");
+  buf[pos] = '\0';
 
-  /* flush output buffer */
+  // writes the data to the socket.
+  printf("%s\n", buf);
   fflush(stdout);
 }
 
@@ -305,8 +142,8 @@ main(int argc, char *argv[])
   /* Register callback */
   /*********************/
   /* register result callback functions */
-  callback_add(recog, CALLBACK_EVENT_SPEECH_READY, status_recready, NULL);
-  callback_add(recog, CALLBACK_EVENT_SPEECH_START, status_recstart, NULL);
+  // callback_add(recog, CALLBACK_EVENT_SPEECH_READY, status_recready, NULL);
+  // callback_add(recog, CALLBACK_EVENT_SPEECH_START, status_recstart, NULL);
   callback_add(recog, CALLBACK_RESULT, output_result, NULL);
 
   /**************************/
